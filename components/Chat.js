a060f48a896d4464f261c343771b1338b0803294
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-10-17 21:01:12 -0400

    implemented firestore for saving messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,40 +1,99 @@
 import React from 'react';
-import { View, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, StyleSheet, LogBox } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+    LogBox.ignoreLogs(['Setting a timer']);
     this.state = {
       messages: [],
+      uid: '',
     }
+    // firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyDAgvR2u0hXwC61-TrQUgrPgwusF44vzzY",
+      authDomain: "test-6be76.firebaseapp.com",
+      databaseURL: "https://test-6be76.firebaseio.com",
+      projectId: "test-6be76",
+      storageBucket: "test-6be76.appspot.com",
+      messagingSenderId: "577953195015",
+    }
+
+    // configure firebase if not already set up
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    // referenecs messages collection in firebase
+    this.referenceMessages = firebase.firestore().collection('messages');
+  };
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // loop through each document from returned query data
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      // pushes returned DB data to messages array, in prep for pushing to state
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    // sorts messages array by createdAt
+    messages.sort((a, b) => b.createdAt - a.createdAt);
+    // sets messages array to state to be displayed
+    this.setState({
+      messages,
+    });
   }
 
   componentDidMount() {
-    // accesses user name from start screen input via navigation parameter
-    let name = this.props.route.params.name;
-    // sets state with sample message and system welcome message
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ' has entered the chat. Welcome!',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    })
+    // listener for messages collection update
+    this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+    // sets user
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+
+      this.setState({
+        uid: user.uid,
+        loggedInText: 'Hello!',
+        user: {
+          _id: user.uid,
+          name: this.props.route.params.name,
+          avatar: 'https://placeimg.com/140/140/any',
+        }
+      });
+    });
+
+    // references the active user's documents
+    this.referenceMessagesUser = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
+
+    // listens for collection updates for the current user
+    this.unsubscribeListUser = this.referenceMessagesUser.onSnapshot(this.onCollectionUpdate);
+  }
+
+  // adds new message to firebase messages collection
+  addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  };
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
   }
 
   renderBubble(props) {
@@ -51,13 +110,17 @@ export default class Chat extends React.Component {
     )
   }
 
-  // appends new message to messages array on send
+  // appends new message to messages array on send and calls addMessage function
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.setState(
+      previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
   }
-
 
   render() {
     return (
@@ -70,9 +133,7 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           // on send, triggers onSend function to append new message to messages in state
           onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+          user={this.state.user}
         />
         {/* if OS is android, adds KeyboardAvoidingView to prevent keyboard from covering input box */}
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
